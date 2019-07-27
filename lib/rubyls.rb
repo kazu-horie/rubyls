758b3d65d4c61dbba0e0dd6978ee951025187a5a
Kazuki Horie <kazukihorie.jp@gmail.com>
2019-07-28 01:07:24 +0900

    Add rubyls.rb

--- lib/rubyls.rb
+++ lib/rubyls.rb
@@ -2,5 +2,21 @@ require "rubyls/version"
 
 module Rubyls
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.ls(path)
+    begin
+      if RUBY_VERSION.to_f >= 2.5
+        Dir.children(path).each do |c|
+          print "#{c}\t"
+        end
+      else
+        Dir.entries(path)[2..-1].each do |c|
+          print "#{c}\t"
+        end
+      end
+      puts
+    rescue Errno::ENOENT
+      puts "ls: #{ARGV[0]}: No such file or directory"
+    end
+  end
 end
