2ba54ec34ac557e47b653120174074d0cd363d49
Kazuki Horie <kazukihorie.jp@gmail.com>
2019-08-19 16:38:21 +0900

    refactor ls method

--- lib/rubyls.rb
+++ lib/rubyls.rb
@@ -4,14 +4,11 @@ module Rubyls
   class Error < StandardError; end
 
   def self.ls(path: '.')
-    begin
-      entries = Dir.entries(path)
-    rescue Errno::ENOENT
-      print "ls: #{path}: No such file or directory\n"
-      return
-    end
+    entries = Dir.entries(path)
     sorted = entries.sort
     general = sorted.reject { |entry| entry.match(/^\./) }
     print general.join("\t") + "\n"
+  rescue Errno::ENOENT
+    print "ls: #{path}: No such file or directory\n"
   end
 end
--- spec/rubyls_spec.rb
+++ spec/rubyls_spec.rb
@@ -1,24 +1,26 @@
 RSpec.describe Rubyls do
-  it "has a version number" do
-    expect(Rubyls::VERSION).not_to be nil
-  end
+  describe '.ls' do
+    it "has a version number" do
+      expect(Rubyls::VERSION).not_to be nil
+    end
 
-  it "output entries within current dir" do
-    expected = output(`ls .`.chomp.gsub(/\n/, "\t") + "\n").to_stdout
+    it "output entries within current dir" do
+      expected = output(`ls .`.chomp.gsub(/\n/, "\t") + "\n").to_stdout
 
-    expect { Rubyls.ls(path: '.') }.to expected
-  end
+      expect { Rubyls.ls(path: '.') }.to expected
+    end
 
-  it "output entries within current dir with no arguments" do
-    expected = output(`ls .`.chomp.gsub(/\n/, "\t") + "\n").to_stdout
+    it "output entries within current dir with no arguments" do
+      expected = output(`ls .`.chomp.gsub(/\n/, "\t") + "\n").to_stdout
 
-    expect { Rubyls.ls }.to expected
-  end
+      expect { Rubyls.ls }.to expected
+    end
 
-  it "output error no such file or directory" do
-    path = 'nonexistent name'
-    expected = output("ls: #{path}: No such file or directory\n").to_stdout
+    it "output error no such file or directory" do
+      path = 'nonexistent name'
+      expected = output("ls: #{path}: No such file or directory\n").to_stdout
 
-    expect { Rubyls.ls(path: path) }.to expected
+      expect { Rubyls.ls(path: path) }.to expected
+    end
   end
 end
